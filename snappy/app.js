1e5c5829b0bb17cbde6abfe7007ee6a89bc5d205
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-01-29 11:40:12 +0530

    [Snappy]Updation was done

--- snappy/app.js
+++ snappy/app.js
@@ -6,3 +6,32 @@ click("sign-up-btn",function(){
 click("log-in-btn",function(){
   logOutUser();
 })
+
+//objects are in js
+var user1 = {
+  id:1,
+  name: "Dhiral",
+  username: "dhiralkaniya",
+  sayHello: function(){
+    console.log("Say Hello Call" + this.name);
+  }
+};
+var user2 = {
+  id:2,
+  name: "Harmeet",
+  username: "paji",
+  sayHello: function(){
+    console.log("Say Hello " + this.name);
+  }
+};
+var users = [
+  user1,user2
+];
+for(var i=0;i<users.length;i++){
+  var user = users[i];
+  console.log(user.sayHello());
+}
+
+var message = new Object();
+message.text= "Hello";
+console.log(message.text);
